f4b72cc347062bc84f2a1b7f594ae884aebef5e2
ritvikshah2000 <ritvikshah2000@gmail.com>
2021-07-26 14:19:50 +0300

    Added humidity feature to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=3d75b4a5c6a427244c046c899fcd232d&query=' + latitude + ',' + longitude + '&units=m'
     request({url: url, json: true}, (error, {body} = {}) => { //if error, then response is empty & vice versa
@@ -8,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error) { //error from weather api
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently  " + body.current.temperature + " degrees celsius out ")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently  " + body.current.temperature + " degrees celsius out. The humidity currently is " + body.current.humidity + "%.")
         }
 
 })
